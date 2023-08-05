e350cabcdf24319335614bf07fcaf956aacda6cf
danielkaboto <danielkabotospdk@gmail.com>
2023-08-05 14:06:58 +0300

    button Animation

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,8 +1,62 @@
-var button= document.getElementsByClassName(".drum");
+var button = document.querySelectorAll(".drum").length;
 
-for( var i=0 ; i < button.length ; i++){
 
-    button[i].addEventListener("click",function(){
-        alert("have been click");
-    });
+
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    ButtonAnimation(event.key);
+})
+
+for (var i=0; i<button ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var choice=this.innerHTML;
+        makeSound(choice);
+        ButtonAnimation(choice);
+});
 }
+function makeSound(key){
+    
+            switch(key){
+                case "w":
+                    var crash = new Audio("sounds/crash.mp3");
+                    crash.play();
+                    break;
+                case "a":
+                    var kick = new Audio("sounds/kick-bass.mp3");
+                    kick.play();
+                    break;
+                case "s":
+                    var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                    break;
+                case "d":
+                    var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+                case "j":
+                   var tom2= new Audio ("sounds/tom-2.mp3");
+                   tom2.play();
+                   break;
+                case "k":
+                    var tom3 =new Audio ("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+                case "l":
+                    var tom4=new Audio ("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+    
+        }
+
+}
+
+function ButtonAnimation(currentValue){
+    var activeButton= document.querySelector("."+currentValue);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+}
+
